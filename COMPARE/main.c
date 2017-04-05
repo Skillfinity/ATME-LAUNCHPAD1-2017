cd0561feeec29dbadb3d2b4f027e842c63e4cea1
Chandraprakash_P <pcprakash695@gmail.com>
2017-04-05 21:45:09 +0530

    compare program

---
+++ COMPARE/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("enter the two numbers\n");
+    scanf("%d %d",&x,&y);
+    if(x>y)
+    {
+        printf("%d is greater than %d\n",x,y);
+    }
+    else if(x<y)
+    {
+        printf("%d is less than %d\n",x,y);
+    }
+    else
+    {
+        printf("%d is equal to %d\n",x,y);
+    }
+    return 0;
+
+}
